1117efa32e5448695f3dd1c5807b41c4018e3ded
lewiswolf <lewiswolstanholme@gmail.com>
2021-04-20 13:05:19 +0100

    generate dataset complete and tested

--- index.py
+++ index.py
@@ -1,53 +1,34 @@
-import sys, os
-import click
+import json, os, sys
+import click					# cli
 
 # add /src to sys.path
 sys.path.insert(1, os.path.join(os.getcwd(), 'src'))
 import generateTargets
 
 # GLOBAL VARS
-SAMPLES_PER_TARGET = 2
-NUM_OF_TARGETS = 200
 PATH_TO_DATASET = os.path.join(os.getcwd(), 'TinySOL')
-
-targets = None
+SAMPLES_PER_TARGET = 10
+NUM_OF_TARGETS = 300
+SAMPLE_RATE = 44100
 
 # set command line flags
 @click.command()
 @click.option('--generate', '-g', is_flag = True, help = 'Generate targets before training.')
 def main(generate):
-	# options
-	GENERATE_TARGETS = generate
+	# generate targets and store metadata
+	targets = None
 
-	# check if targets folder is populated
-	if (len(os.listdir('./targets')) - 1 != NUM_OF_TARGETS or GENERATE_TARGETS):
-		# check if dataset exists
-		if (not os.path.isdir(PATH_TO_DATASET)):
-			print('Dataset not found.')
-			sys.exit()
-		# generate targets
-		print('Generating source seperation targets... 🎯')
-		targets = generateTargets.generateTargets(PATH_TO_DATASET, SAMPLES_PER_TARGET)
+	if (generate):
+		targets = generateTargets.generateTargets()
 	else:
-		targets = None # load targets metadata
-
+		try:
+			# load targets metadata
+			targets = json.load(open(os.path.join(os.getcwd(), 'targets/metadata.json'), 'r'))
+		except:
+			targets = generateTargets.generateTargets()
+		# if the project settings and data settings don't align, regenerate
+		if (targets['SAMPLES_PER_TARGET'] != SAMPLES_PER_TARGET or targets['NUM_OF_TARGETS'] != NUM_OF_TARGETS or targets['SAMPLE_RATE'] != SAMPLE_RATE):
+			targets = generateTargets.generateTargets()
 
 if __name__ == '__main__':
     main()
-
-	# import pandas as pd		# databases
-	# import torch				# pytorch
-	# import torchaudio			# handling audio
-
-	# # import metadata
-	# metadata = pd.read_csv('./TinySOL_2020/TinySOL_metadata.csv', engine='python')
-
-	# # for i in range(len(metadata)):
-	# waveform, sampleRate = torchaudio.load('./TinySOL_2020/TinySOL/Brass/BTb/ordinario/BTb-ord-A#1-ff.wav')
-	# # waveform, sampleRate = torchaudio.load_wav(f'./TinySOL_2020/{metadata['Path'][i]}')
-	# spectrogram = torchaudio.transforms.MelSpectrogram(
-	# 	sample_rate = sampleRate,
-	# 	n_mels = 128,
-	# 	hop_length = 2048,
-	# 	pad = int(((44100 * 4) - waveform.size()[1]) / 2)
-	# )(waveform[0])
--- src/generateTargets.py
+++ src/generateTargets.py
@@ -1,25 +1,107 @@
-import os, sys
-# import itertools, math
-import pandas as pd		# databases
-import numpy as np		# maths
+import json, os, random, sys
+import click							# cli
+import pandas as pd						# databases
+import numpy as np						# maths
+import soundfile as sf					# audio i/o
+import librosa							# audio tools
 
-# class Sample:
-# 	def __init__(self):
+# import global vars
+from index import PATH_TO_DATASET, NUM_OF_TARGETS, SAMPLES_PER_TARGET, SAMPLE_RATE
 
-def importMetadata(PATH_TO_DATASET):
-	pathToMetadata = os.path.join(PATH_TO_DATASET, 'TinySOL_metadata.csv')
+class Sample:
+	def __init__(self):
+		self.instruments = []
+		self.labels = []
+		self.sampleFilepaths = []
+		self.renderedFilepath = ''
+
+	def combineSamples(self, index):
+		output = np.zeros(SAMPLE_RATE * 4)
+		tmpList = []
+		longest_sample = 0
+
+		# populate tmp list and find longest sample
+		for i in range(SAMPLES_PER_TARGET):
+			audio, sr = sf.read(os.path.join(PATH_TO_DATASET, self.sampleFilepaths[i]))
+			if (sr != SAMPLE_RATE):
+				librosa.resample(audio, sr, SAMPLE_RATE)
+			tmpList += [audio]
+			if (len(tmpList[i]) > longest_sample):
+				longest_sample = len(tmpList[i])
+
+		# pad and combine samples into a numpy array
+		tmpNP = np.zeros((longest_sample))
+		for i in range(SAMPLES_PER_TARGET):
+			prefix = random.randint(0, longest_sample - len(tmpList[i]))
+			for j in range(len(tmpList[i])):
+				tmpNP[j + prefix] += tmpList[i][j]
+
+		# reduce amplitude and trim output
+		tmpNP /= SAMPLES_PER_TARGET
+		trim = (longest_sample - output.shape[0]) // 2
+		for i in range(output.shape[0]):
+			output[i] = tmpNP[i + trim]
+
+		# set metadata and export target
+		outputPath = os.path.join(os.getcwd(), f'targets/target_{index}.wav')
+		self.renderedFilepath = outputPath
+		sf.write(outputPath, output, SAMPLE_RATE)
+	
+	def getMetadata(self):
+		return {
+			'labels': self.labels,
+			'filepath': self.renderedFilepath,
+		}
+
+
+def generateTargets():
+	print('Generating source seperation targets... 🎯')
+
+	# get dataset metadata
 	try:
-		metadata = pd.read_csv(pathToMetadata, engine='python')
+		datasetMetadata = pd.read_csv(
+			os.path.join(PATH_TO_DATASET, 'TinySOL_metadata.csv'),
+			engine='python'
+		)
 	except:
 		print('Metadata not found.')
 		sys.exit()
-	return metadata
 
-def generateTargets(PATH_TO_DATASET, SAMPLES_PER_TARGET):
-	metadata = importMetadata(PATH_TO_DATASET)
-	# subsets = []
-	# for i in range(math.floor(len(metadata) / SAMPLES_PER_TARGET)):
-	# 	tmp = []
-	# 	for j in range(SAMPLES_PER_TARGET):
-	# 		tmp.append(metadata['Path'][i * SAMPLES_PER_TARGET + j])
-	# 	subsets += [tmp]
+	# initialise targets metadata
+	targetsMetadata = {
+		"SAMPLES_PER_TARGET": SAMPLES_PER_TARGET,
+		"NUM_OF_TARGETS": NUM_OF_TARGETS,
+		"SAMPLE_RATE": SAMPLE_RATE,
+		"targets": []
+	}
+
+	with click.progressbar(length = NUM_OF_TARGETS) as bar:
+		for i in range(NUM_OF_TARGETS):
+			sample = Sample()
+			for j in range(SAMPLES_PER_TARGET):
+				# generate a random datasample
+				index = random.randint(0, len(datasetMetadata) - 1)
+				# regenerate if there are more than 3 instances of this instrument
+				while(sample.instruments.count(datasetMetadata['Instrument (abbr.)'][index]) >= 3):
+					index = random.randint(0, len(datasetMetadata) - 1)
+				# append metadata to class
+				sample.instruments += [datasetMetadata['Instrument (abbr.)'][index]]
+				sample.sampleFilepaths += [datasetMetadata['Path'][index]]
+				sample.labels += [[
+					datasetMetadata['Instrument (in full)'][index], 
+					datasetMetadata['Pitch'][index], 
+					datasetMetadata['Dynamics'][index]
+				]]
+				
+			# combine samples
+			sample.combineSamples(i)
+
+			# append metadata to output json and update progressbar
+			targetsMetadata['targets'] += [sample.getMetadata()]
+			bar.update(1)
+
+	# export metadata as json
+	with open(os.path.join(os.getcwd(), 'targets/metadata.json'), 'w') as json_file:
+		json.dump(targetsMetadata, json_file)
+
+	return targetsMetadata

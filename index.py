9b0291be5011f28714f3847464d677db03eb5e05
lewiswolf <lewiswolstanholme@gmail.com>
2021-04-20 23:20:25 +0100

    cleanup and preparing for tomorrow

--- index.py
+++ index.py
@@ -3,8 +3,9 @@ import click					# cli
 
 # add /src to sys.path
 sys.path.insert(1, os.path.join(os.getcwd(), 'src'))
-from generateTargets import generateTargets
 from settings import NUM_OF_TARGETS, SAMPLES_PER_TARGET, SAMPLE_RATE
+from generateTargets import generateTargets
+from nn import train_model
 
 # set command line flags
 @click.command()
@@ -12,7 +13,6 @@ from settings import NUM_OF_TARGETS, SAMPLES_PER_TARGET, SAMPLE_RATE
 def main(generate):
 	# generate targets and store metadata
 	targets = None
-
 	if (generate):
 		targets = generateTargets()
 	else:
@@ -22,8 +22,13 @@ def main(generate):
 		except:
 			targets = generateTargets()
 		# if the project settings and data settings don't align, regenerate
+		# this only happens if the import works
 		if (targets['SAMPLES_PER_TARGET'] != SAMPLES_PER_TARGET or targets['NUM_OF_TARGETS'] != NUM_OF_TARGETS or targets['SAMPLE_RATE'] != SAMPLE_RATE):
 			targets = generateTargets()
+	print('Dataset loaded! 🗄')
+
+	# train the model
+	train_model(targets['targets'])
 
 if __name__ == '__main__':
     main()

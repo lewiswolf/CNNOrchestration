83da24bdf5bbdaafbaf2f81ecc96ff6fe9401a24
lewiswolf <lewiswolstanholme@gmail.com>
2021-04-20 13:42:30 +0100

    code splitting for later

---
+++ src/target.py
@@ -0,0 +1,52 @@
+import os, random
+import numpy as np						# maths
+import soundfile as sf					# audio i/o
+import librosa							# audio tools
+
+# import global vars
+from index import PATH_TO_DATASET, NUM_OF_TARGETS, SAMPLES_PER_TARGET, SAMPLE_RATE
+
+class Target:
+	def __init__(self):
+		self.instruments = []
+		self.labels = []
+		self.sampleFilepaths = []
+		self.renderedFilepath = ''
+
+	def combine_for_training(self, index):
+		output = np.zeros(SAMPLE_RATE * 4)
+		tmpList = []
+		longest_sample = 0
+
+		# populate tmp list and find longest sample
+		for i in range(SAMPLES_PER_TARGET):
+			audio, sr = sf.read(os.path.join(PATH_TO_DATASET, self.sampleFilepaths[i]))
+			if (sr != SAMPLE_RATE):
+				librosa.resample(audio, sr, SAMPLE_RATE)
+			tmpList += [audio]
+			if (len(tmpList[i]) > longest_sample):
+				longest_sample = len(tmpList[i])
+
+		# pad and combine samples into a numpy array
+		tmpNP = np.zeros((longest_sample))
+		for i in range(SAMPLES_PER_TARGET):
+			prefix = random.randint(0, longest_sample - len(tmpList[i]))
+			for j in range(len(tmpList[i])):
+				tmpNP[j + prefix] += tmpList[i][j]
+
+		# reduce amplitude and trim output
+		tmpNP /= SAMPLES_PER_TARGET
+		trim = (longest_sample - output.shape[0]) // 2
+		for i in range(output.shape[0]):
+			output[i] = tmpNP[i + trim]
+
+		# set metadata and export target
+		outputPath = os.path.join(os.getcwd(), f'targets/target_{index}.wav')
+		self.renderedFilepath = outputPath
+		sf.write(outputPath, output, SAMPLE_RATE)
+	
+	def getMetadata(self):
+		return {
+			'labels': self.labels,
+			'filepath': self.renderedFilepath,
+		}
